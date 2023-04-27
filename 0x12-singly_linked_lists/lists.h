72f41db1e24a709e702e9db2d9d54e4ac17da644
Yasmine <yasmineelamar@gmail.com>
2023-04-27 11:56:13 -0700

    Add node at the end

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-
-#include <stdlib.h>
-#include <string.h>
-#include <stddef.h>
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -23,6 +17,7 @@ struct list_s *next;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 list_t *add_node(list_t **head, const char *str);
 void free_list(list_t *head);
 
