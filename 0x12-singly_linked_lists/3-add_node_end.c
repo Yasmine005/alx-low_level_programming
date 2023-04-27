1306cef4f47efee073fc75d89a42600d9be96e93
Yasmine <yasmineelamar@gmail.com>
2023-04-27 11:26:17 -0700

    3. Add node at the end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - ...
+ * @head: ...
+ * @str: ...
+ *
+ * Return: ...
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *new;
+list_t *end_node = *head;
+size_t len = 0;
+
+while (str && str[len])
+len++;
+
+new = malloc(sizeof(list_t));
+if (!new)
+return (NULL);
+
+new->str = strdup(str);
+new->len = len;
+new->next = NULL;
+
+if (*head == NULL)
+{
+*head = new;
+return (new);
+}
+
+while (end_node->next)
+end_node = end_node->next;
+
+end_node->next = new;
+
+return (new);
+}
---
+++ 0x12-singly_linked_lists/4-main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    list_t *head;
+
+    head = NULL;
+    add_node(&head, "Bob");
+    add_node(&head, "&");
+    add_node(&head, "Kris");
+add_node_end(&head, "love");
+    add_node_end(&head, "asm");
+    print_list(head);
+    free_list(head);
+    head = NULL;
+    return (0);
+}
